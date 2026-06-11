b3bf2ac3cfe301d963909584b9c6b00d67db3e4a
Rohith Gilla <gillarohith1@gmail.com>
2026-06-11 14:18:47 +0530

    chore: eslint react-hooks rules for islands

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,15 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config([
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  {
+    files: ["**/*.tsx"],
+    plugins: { "react-hooks": reactHooks },
+    rules: reactHooks.configs.flat["recommended-latest"].rules,
+  },
   {
     ignores: ["dist/", ".astro/", "docs/"],
   },
